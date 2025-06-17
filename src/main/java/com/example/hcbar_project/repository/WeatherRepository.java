114311c5ab7225e2358a113cd2435c0d5ceb314a
Mai Yamamoto <maiyamamoto2002@gmail.com>
2025-06-17 17:26:19 +0900

    edited by mai

--- src/main/java/com/example/hcbar_project/repository/WeatherRepository.java
+++ src/main/java/com/example/hcbar_project/repository/WeatherRepository.java
@@ -6,12 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
     // 日付で天気を取得
     Weather findByDate(java.time.LocalDate date);
-}package com.example.hcbar_project.repository;
-
-import com.example.hcbar_project.model.Weather;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
-    // 日付で天気を取得
-    Weather findByDate(java.time.LocalDate date);
 }
