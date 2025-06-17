1f4b089f37435690249ffdf6b777591f9b1498ce
Mai Yamamoto <maiyamamoto2002@gmail.com>
2025-06-17 20:37:25 +0900

    サイドメニュー真ん中変かも

---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,19 @@
+    function navigate(target) {
+        switch (target) {
+            case 'forecast':
+                window.location.href = '/forecast';
+                break;
+            case 'user':
+                window.location.href = '/users/list';
+                break;
+            case 'product':
+                window.location.href = '/products';
+                break;
+            case 'salesInput':
+                window.location.href = '/sales/input';
+                break;
+            case 'salesList':
+                window.location.href = '/sales/list';
+                break;
+        }
+    }
