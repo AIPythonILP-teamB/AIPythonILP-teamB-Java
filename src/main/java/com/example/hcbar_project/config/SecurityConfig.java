c09ccb7ee94b9e9a7affa9ccb62042409ddc628a
Mai Yamamoto <maiyamamoto2002@gmail.com>
2025-06-23 15:39:40 +0900

    各ページに名前表示

--- src/main/java/com/example/hcbar_project/config/SecurityConfig.java
+++ src/main/java/com/example/hcbar_project/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/default", "/reset-password/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/sale/**").hasRole("USER")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
